91efe0dbaea6a0050f9d6910d0b9c4e4f8c9cc8c
ilhomidin <itilhomidin@yandex.com>
2021-08-04 23:48:35 +0500

    Remove bot package and add base bot factory method

---
+++ bot.py
@@ -0,0 +1,44 @@
+"""
+This module provides a factory method that creates an instance of the bot.
+"""
+from deta import Deta
+import telebot
+
+
+def get_bot(bot_token: str, deta_project_key: str) -> telebot.AsyncTeleBot:
+    """
+    Creates an instance of the bot.
+
+    Args:
+        bot_token: The bot's token.
+        deta_project_key: The deta project key.
+
+    Returns:
+        telebot.AsyncTeleBot: bot instance.
+    """
+    deta = Deta(deta_project_key)
+    users = deta.Base("users")
+    bot = telebot.TeleBot(bot_token, parse_mode="Markdown")
+
+    @bot.message_handler(commands=["start"])
+    def start(message: telebot.types.Message):
+        """
+        Handler for /start command.
+
+        Send welcome message.
+        Adds the user to the database or sets the default state.
+        """
+        try:
+            users.insert({"state": None}, str(message.chat.id))
+        except Exception:  # pylint: disable=W0703
+            users.update({"state": None}, str(message.chat.id))
+        bot.send_message(
+            message.chat.id,
+            "👋 Hi, i will help you with ```geocoding``` - \
+            finding the coordinates of a place by name.",
+        )
+
+    return bot
+
+
+__all__ = ["get_bot"]
