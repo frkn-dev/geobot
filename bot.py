6c0369fd3a9c200445e7106e550bef7e409cac71
ilhomidin <itilhomidin@yandex.com>
2021-08-07 16:21:28 +0500

    Add handlers for simple search process

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 """
 This module provides a factory method that creates an instance of the bot.
 """
-from deta import Deta
+import requests
 import telebot
+from deta import Deta
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 
 def get_bot(bot_token: str, deta_project_key: str) -> telebot.AsyncTeleBot:
@@ -38,6 +40,76 @@ def get_bot(bot_token: str, deta_project_key: str) -> telebot.AsyncTeleBot:
                 photo=cover,
                 caption="👋 Hi, i will help you with `geocoding` - finding the coordinates of a place by name.",
             )
+
+    @bot.message_handler(commands=["search"])
+    def welcome_search(message: telebot.types.Message):
+        """
+        Handler for /search command.
+
+        Send a welcome message to search.
+        """
+        users.update({"state": "search"}, str(message.chat.id))
+        bot.send_message(
+            message.chat.id,
+            "🔍 Send any name of the place to start the search.",
+        )
+
+    @bot.message_handler(
+        func=lambda msg: users.get(str(msg.chat.id))["state"] == "search"
+    )
+    def search(message: telebot.types.Message):
+        """
+        A simple nominatim search for a given query text.
+        """
+        try:
+            locations = requests.get(
+                "https://nominatim.openstreetmap.org/search",
+                {"q": message.text, "format": "json"},
+            ).json()
+            if len(locations) == 0:
+                bot.send_message(message.chat.id, "🔍 No results found.")
+            else:
+                bot.delete_message(message.chat.id, message.message_id)
+                bot.send_message(
+                    message.chat.id,
+                    "🌎 I found these places by searching for \"{}\":".format(message.text),
+                    reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton(
+                                    location["display_name"],
+                                    callback_data="{}:{}".format(
+                                        location["lat"], location["lon"]
+                                    ),
+                                )
+                            ]
+                            for location in locations
+                        ]
+                    ),
+                )
+        except Exception as exc:  # pylint: disable=W0703
+            bot.send_message(message.chat.id, "🤔 Oops. Something went wrong.")
+            raise exc
+        users.update({"state": None}, str(message.chat.id))
+
+    @bot.callback_query_handler(func=lambda call: call.data)
+    def show_location(callback: telebot.types.CallbackQuery):
+        """
+        Handler for inline keyboard buttons.
+
+        Sends a location to the user.
+        """
+        if ":" in callback.data:
+            latitude, longitude = callback.data.split(":")
+        bot.delete_message(
+            callback.message.chat.id, callback.message.message_id
+        )
+        bot.send_location(
+            callback.message.chat.id,
+            latitude,
+            longitude,
+            reply_markup=callback.message.reply_markup,
+        )
 
     return bot
 
