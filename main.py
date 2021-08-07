dbef2bca9a986f3cb777de40f728837796e496a0
ilhomidin <itilhomidin@yandex.com>
2021-08-07 17:13:24 +0500

    Add webhook handler

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 """
 Webhooks handler for bot.
 """
+from os import environ
+
+import telebot
 import flask
+from bot import get_bot
 
 
 app = flask.Flask(__name__)
+bot = get_bot(
+    environ.get("BOT_TOKEN"),
+    environ.get("DETA_PROJECT_KEY"),
+)
+bot.set_webhook(environ.get("WEBHOOK_URL") + "/" + environ.get("BOT_TOKEN"))
 
 
 @app.route("/")
@@ -13,3 +22,18 @@ def index():
     Display a small page about the bot with a link.
     """
     return flask.render_template("index.html")
+
+
+@app.route("/<token>", methods=["POST"])
+def webhook(token: str):
+    """
+    Handle webhooks.
+    """
+    data = flask.request.get_json(silent=True, force=True)
+    if data:
+        try:
+            update = telebot.types.Update.de_json(data)
+            bot.process_new_updates([update])
+        except Exception:  # pylint: disable=broad-except
+            ...
+    return "ok"
